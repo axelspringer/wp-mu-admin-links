de0751cb4ea81d841b73be13feb4c84c445c1859
Mathias Witt <mathias.witt@axelspringer.de>
2018-03-12 10:34:46 +0100

    PEA-6547: load links on specific environment

--- asse-admin-links.php
+++ asse-admin-links.php
@@ -2,18 +2,21 @@
 
 /**
  * Display links in the Admin Bar
- * 
+ *
  * Use ASSE_ADMIN_LINKS as constant
- * 
+ *
  * @param $wpAdminBar
  */
 function customAdminToolbarLink($wpAdminBar)
 {
-    if (!defined('ASSE_ADMIN_LINKS')) {
+    if (!defined('ASSE_ADMIN_LINKS')
+        || !defined("WP_ENVIRONMENT")
+        || !array_key_exists(WP_ENVIRONMENT, ASSE_ADMIN_LINKS)
+    ) {
         return;
     }
 
-    foreach (ASSE_ADMIN_LINKS as $title => $href) {
+    foreach (ASSE_ADMIN_LINKS[WP_ENVIRONMENT] as $title => $href) {
         $id = 'asse-' . strtolower($title) . '-link';
         $args = array(
             'id' => $id,
